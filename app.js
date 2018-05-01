721e982ec2a7c719efdb27860d3870008bc85637
john-w-c <33558191+john-w-c@users.noreply.github.com>
2018-05-01 12:39:37 -0700

    Add greeting route, change hello route to display gif

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello-squirrel', function(req, res) {
-  res.send('Hello Squirrel');
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/hello-gif', function(req, res) {
+  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+  res.render('hello-gif', {gifUrl: gifUrl})
 });
+
+app.get('/greetings/:name', function(req, res) {
+  var name = req.params.name;
+  res.render('greetings', {name: name});
+})
 
 app.listen(3000, function() {
   console.log('Gif Search listening on port localhost:3000!');
