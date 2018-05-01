6db5e4eb3889ccc763cccbd754bf824208e20e63
john-w-c <33558191+john-w-c@users.noreply.github.com>
2018-05-01 13:41:54 -0700

    Add very basic stylesheet, finished tutorial

--- app.js
+++ app.js
@@ -1,38 +1,50 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var http = require('http');
+var giphy = require('giphy-api')(); // todo: what's with the second parens?
 
 var app = express();
+app.use(express.static('public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-  console.log(req.query.term);
-  var queryString = req.query.term;
-  // encode queryString to remove whitespace and restricted chars
-  var term = encodeURIComponent(queryString);
-  // put search term into Giphy API search URL
-  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
+  if (req.query.term === undefined) {
+    giphy.trending(function(err, response) {
+      res.render('home', {gifs: response.data});
+    });
+  } else {
+    giphy.search(req.query.term, function(err, response) {
+      res.render('home', {gifs: response.data});
+    });  
+  }
   
-  http.get(url, function(response) {
-    // set encoding of response to utf-8
-    response.setEncoding('utf-8');
+  // console.log(req.query.term);
+  // var queryString = req.query.term;
+  // // encode queryString to remove whitespace and restricted chars
+  // var term = encodeURIComponent(queryString);
+  // // put search term into Giphy API search URL
+  // var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
+  
+  // http.get(url, function(response) {
+  //   // set encoding of response to utf-8
+  //   response.setEncoding('utf-8');
 
-    var body = '';
+  //   var body = '';
 
-    response.on('data', function(d) {
-      // continuously update stream w/ data from Giphy
-      body += d;
-    });
+  //   response.on('data', function(d) {
+  //     // continuously update stream w/ data from Giphy
+  //     body += d;
+  //   });
 
-    response.on('end', function() {
-      // when data fully recieved, parse into JSON
-      var parsed = JSON.parse(body);
-      // render home template, pass gif data into template
-      res.render('home', {gifs: parsed.data});
-    });
-  });
+  //   response.on('end', function() {
+  //     // when data fully recieved, parse into JSON
+  //     var parsed = JSON.parse(body);
+  //     // render home template, pass gif data into template
+  //     res.render('home', {gifs: parsed.data});
+  //   });
+  // });
 })
 
 app.get('/hello-gif', function(req, res) {
